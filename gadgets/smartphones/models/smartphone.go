a355282f85c830796646a74c099bc832bcebe456
Natalia Gonzalez <nataliaisagonzalez26@gmail.com>
2021-07-09 12:52:23 -0500

    First commit

---
+++ gadgets/smartphones/models/smartphone.go
@@ -0,0 +1,12 @@
+package models
+
+//smarthphones model structure for smartphone
+type Smartphone struct {
+	Id int64 //similar to a long in java
+	Name string
+	Price int
+	CountryOrigin string
+	Os string
+}
+
+// CMD + alt + L // gofmt -w .
